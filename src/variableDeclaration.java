30cca748e646bd84f69cd6daff89ad28d176ca43
Kramer <kramermwangala@gmail.com>
2024-08-06 20:12:07 -0700

    day 1 learning java

---
+++ src/variableDeclaration.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+
+
+public class variableDeclaration {
+
+    public static void main(String[] args) {
+        int i;
+        i = 5;
+
+        System.out.println(i);
+    }
+}
